592614a87542e0204758cf022a40541d59e48cc3
Gabriel Goncalves <gabrielraeder@outlook.com.br>
2023-04-23 15:31:38 -0300

    trybeNews-FastAPI - feat: uses query at route title search

--- trybeNews-fastAPI-scraper/src/main.py
+++ trybeNews-fastAPI-scraper/src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 from api.controllers.news_controller import NewsController
 from api.middlewares import Middleware
 
@@ -7,27 +7,27 @@ app = FastAPI()
 news_controller = NewsController()
 
 
-@app.post("/scrape-news", status_code=201)
+@app.post("/scrape-news", status_code=201, tags=["scrape"])
 async def scrape(amount: int = Body(embed=True, default=None)):
     Middleware.amount(amount)
     return news_controller.scrape_news(amount)
 
 
-@app.get("/top-categories")
+@app.get("/top-categories", tags=["search-filters"])
 async def categories():
     return news_controller.top_categories()
 
 
-@app.get("/title/{title}")
-async def title(title: str):
-    return news_controller.title_search(title)
+@app.get("/title/", tags=["search-filters"])
+async def title(q: str = Query(default="", max_length=50)):
+    return news_controller.title_search(q)
 
 
-@app.get("/category/{category}")
+@app.get("/category/{category}", tags=["search-filters"])
 async def category(category: str):
     return news_controller.category_search(category)
 
 
-@app.get("/date/{yyyy-mm-dd}")
+@app.get("/date/{yyyy-mm-dd}", tags=["search-filters"])
 async def date(date: str):
     return news_controller.date_search(date)
